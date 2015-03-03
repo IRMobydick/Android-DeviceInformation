1c6ba232a653f83c289d2d2e4155adbb0d98f813
Akexorcist <akexorcist@gmail.com>
2015-03-03 19:23:48 +0700

    Bug Fixed

--- app/src/main/java/app/akeorcist/deviceinformation/constants/Key.java
+++ app/src/main/java/app/akeorcist/deviceinformation/constants/Key.java
@@ -8,4 +8,6 @@ public class Key {
     public static final String PARSE_APP_ID = "NbdiZ4IoaaM4YAyh8y7pDtUdZigTbZfHVsSDz5uE";
     public static final String PARSE_CLIENT_ID = "ZNj0ILJqMwJNmyp7iIMABkqupvtbSkaBChwRmwOP";
 
+    // Yo!
+
 }
